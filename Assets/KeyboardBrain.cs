11542c85e36281e7fb0c4a6d87157c8c831189f0
oliver.pruchnicki <35111378+SquidSushi@users.noreply.github.com>
2023-09-27 12:18:19 +0200

    IT'D DONE I THINK :smile_cat:

--- Assets/KeyboardBrain.cs
+++ Assets/KeyboardBrain.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,10 @@ public class KeyboardBrain : MonoBehaviour
     private string currentText = "DefaultText";
     public TMPro.TextMeshPro tmp;
     private GameObject go;
+    public GameObject prefabSquid;
+    public GameObject prefabFriends;
+    public GameObject prefabDeer;
+    public GameObject prefabMonkey;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +24,7 @@ public class KeyboardBrain : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void UpdateTextDisplay()
@@ -48,9 +53,12 @@ public class KeyboardBrain : MonoBehaviour
     public void EnterKey()
     {
         //TODO all the magic
+        EvaluateCommand();
         ClearText();
         UpdateTextDisplay();
     }
+
+
 
     public void ClearSpawnedObjects()
     {
@@ -58,6 +66,47 @@ public class KeyboardBrain : MonoBehaviour
         foreach (Transform t in gameObjects)
         {
             Destroy(t.gameObject);
+        }
+    }
+
+    private void EvaluateCommand()
+    {
+        string[] words = this.currentText.Split(' ');
+        if (words == null)
+        {
+            return;
+        }
+        GameObject selectedPrefab = null;
+        Debug.Log("Enter Pressed");
+        switch (words[0])
+        {
+            case "SQUID":
+                selectedPrefab = this.prefabSquid;break;
+            case "MONKEY":
+                selectedPrefab = prefabMonkey;break;
+            case "DEER":
+                selectedPrefab = prefabDeer;break;
+            case "FRIENDS":
+                selectedPrefab = prefabFriends;break;
+            default:
+                break;
+        }
+        if (selectedPrefab != null)
+        {
+            int toSpawn = 1;
+            if (words.Length > 1)
+            {
+                toSpawn = int.Parse(words[1]);
+            }
+
+            for (int i = 0; i < toSpawn; i++)
+            {
+                GameObject newest = GameObject.Instantiate(selectedPrefab);
+                newest.transform.parent = this.go.transform;
+                newest.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 1;
+                newest.transform.Translate(0, i * 2, 0);
+                newest.transform.LookAt(Camera.main.transform.position);
+            }
         }
     }
 }
